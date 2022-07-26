fbe869e4bd6697454d12ec786da69e1c5a7c7f99
hellen1221 <hsrhee00@gmail.com>
2022-07-26 14:36:51 +0900

    깃올리기 테스트2

--- f3.py
+++ f3.py
@@ -1,3 +1,4 @@
+# 함수작성과 깃올리기 테스트2
 #function3.py
 
 a=1.2
@@ -6,7 +7,9 @@ a=2.3
 print(id(a))
 
 
-lst = [1,2,3]
+lst = [1,2,3,4,5,6]
 print(id(lst))
-lst.append(4)
+lst.append(7)
 print(id(lst))
+print(lst)
+print("The Ends---")
